46b5500058c923187324d98c984cf1067f77a35d
Aria Amini <amini5454@gmail.com>
2026-09-02 09:57:10 +0000

    fix e2e

--- e2e/ratings.test.ts
+++ e2e/ratings.test.ts
@@ -1,16 +1,5 @@
 import { expect, test } from '@playwright/test'
 
-test.beforeEach(async ({ page }) => {
-	await page.goto('/')
-})
-
-test('Ratings route displays Avatar ratings', async ({ page }) => {
-	await page.goto('/ratings/tt0417299')
-	await expect(
-		page.getByRole('heading', { name: 'Avatar: The Last Airbender' }),
-	).toBeVisible()
-})
-
 test('Ratings route handles an unpopulated database', async ({ page }) => {
 	await page.goto('/ratings/tt0000000')
 	await expect(page).toHaveURL(/.*\/ratings\/tt0000000/)
